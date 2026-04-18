44bdd8175a2b7788e03a3f45cc16b1c44215c0f5
Gloomsfield <amory@acosta.boo>
2026-04-17 17:38:49 -0700

    add enemy class, implement enemy creation function

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+public class Enemy {
+	public string SpriteIndex;
+
+	[JsonProperty("name", DefaultValueHandling = DefaultValueHandling.Populate)]
+	public string Name = "unnamed enemy";
+	[JsonProperty("hp", DefaultValueHandling = DefaultValueHandling.Populate)]
+	public int HP = 20;
+	[JsonProperty("speed", DefaultValueHandling = DefaultValueHandling.Populate)]
+	public int Speed = 5;
+	[JsonProperty("damage", DefaultValueHandling = DefaultValueHandling.Populate)]
+	public int Damage = 5;
+
+	public static Dictionary<string, Enemy> CreateEnemiesFromJson(string jsonPath) {
+		TextAsset enemyJson = Resources.Load<TextAsset>(jsonPath);
+
+		List<Enemy> enemies = JsonConvert.DeserializeObject<List<Enemy>>(enemyJson);
+
+		Dictionary<string, Enemy> enemyDictionary = new Dictionary<string, Enemy>();
+
+		foreach(Enemy enemy in enemies) {
+			if(enemyDictionary.TryAdd(enemy.name, enemy)) { continue; }
+
+			Debug.LogError($"{enemy.name} is defined multiple times in {jsonPath}!");
+		}
+
+		return enemyDictionary;
+	}
+}
+
