a86fe940d6b458c476450cf6056d16855440ed3e
Gloomsfield <amory@acosta.boo>
2026-04-25 16:12:38 -0700

    remove monobehavior dependence

---
+++ Assets/Scripts/Core/Statuses.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+
+namespace Statuses {
+    public enum Status {
+        [Description("Success")]
+        SUCCESS = 0,
+        [Description("Enemy stat redefinition error")]
+        ENEMY_STAT_REDEFINITION,
+    }
+}
+
+public static class StatusExtensions {
+    // enum-string association method provided by stackoverflow user
+    // Glennular at https://stackoverflow.com/a/630900
+    public static string StatusString(this Statuses.Status s) {
+        DescriptionAttribute[] attributes = (DescriptionAttribute[])s
+            .GetType()
+            .GetField(s.ToString())
+            .GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+        if(attributes.Length == 0) { return "UNDEFINED STATUS"; }
+
+        return attributes[0].Description;
+    }
+}
