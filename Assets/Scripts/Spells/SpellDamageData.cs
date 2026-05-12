cdc7368967fa5fc78428c9caecde31e54b86473f
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-05-11 17:09:39 -0700

    Add data structures for json ingestion

---
+++ Assets/Scripts/Spells/SpellDamageData.cs
@@ -0,0 +1,8 @@
+namespace Spells
+{
+    public class SpellDamageData
+    {
+        public string amount { get; set; }
+        public string type { get; set; }
+    }
+}
---
+++ Assets/Scripts/Spells/SpellProjectileData.cs
@@ -0,0 +1,10 @@
+namespace Spells
+{
+    public class SpellProjectileData
+    {
+        public string trajectory { get; set; }
+        public string speed { get; set; }
+        public string lifetime { get; set; }
+        public int sprite { get; set; } = 0;
+    }
+}
