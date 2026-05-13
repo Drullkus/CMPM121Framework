543385940ff76b7c59a00d874ee3f2f47b54c1c7
Gloomsfield <amory@acosta.boo>
2026-05-13 16:29:18 -0700

    add Spells.Stat skeleton class

---
+++ Assets/Scripts/Spells/SpellStat.cs
@@ -0,0 +1,16 @@
+namespace Spells {
+
+	public class Stat {
+		
+		private string _value;
+
+		public Stat(string value) {
+			_value = value;
+		}
+
+		// TODO - void ApplyModifier(Modifier modifier)
+
+	}
+
+}
+
