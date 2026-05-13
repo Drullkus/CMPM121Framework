af74082e97f7338473467dfe8433c4f00820fc73
Gloomsfield <amory@acosta.boo>
2026-05-13 16:25:13 -0700

    write constructor for ProjectileData

--- Assets/Scripts/Spells/ProjectileData.cs
+++ Assets/Scripts/Spells/ProjectileData.cs
@@ -12,5 +12,17 @@ namespace Spells
         private readonly string _speed;
         private readonly string _lifetime;
         private readonly int _sprite;
+
+		public ProjectileData(
+			Trajectory trajectory,
+			string speed,
+			string lifetime,
+			int sprite
+		) {
+			_trajectory = trajectory;
+			_speed = speed;
+			_lifetime = lifetime;
+			_sprite = sprite;
+		}
     }
 }
