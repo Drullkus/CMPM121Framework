5915da2537d5470e452ca9ad28d8e34ad4e4d2cf
Gloomsfield <amory@acosta.boo>
2026-05-13 19:41:25 -0700

    clean up SpellData for json deserialization

--- Assets/Scripts/Spells/SpellData.cs
+++ Assets/Scripts/Spells/SpellData.cs
@@ -1,23 +1,38 @@
-namespace Spells
-{
-    public class SpellData
-    {
-        public string name { get; set; }
-        public string description { get; set; }
-        public int icon { get; set; }
-        public string N { get; set; }
-        public SpellDamageData damage { get; set; }
-        public string secondaryDamage { get; set; }
-        public string manaCost { get; set; }
-        public string cooldown { get; set; }
-        public ProjectileData projectile { get; set; }
-        public ProjectileData secondaryProjectile { get; set; }
-        
-        public string delay { get; set; }
-        public string damageMultiplier { get; set; }
-        public string manaMultiplier { get; set; }
-        public string speedMultiplier { get; set; }
-        public string manaAdder { get; set; }
-        
+using Newtonsoft.Json;
+
+namespace Spells {
+
+    public class SpellData {
+
+		[JsonProperty("name")]
+		private readonly string _name;
+		[JsonProperty("description")]
+		private readonly string _description;
+		[JsonProperty("icon")]
+		private readonly string _icon;
+
+		[JsonProperty("damage")]
+		private readonly SpellDamageData _primaryDamage;
+		[JsonProperty("secondary_damage")]
+		private readonly SpellDamageData _secondaryDamage;
+
+		[JsonProperty("projectile")]
+		private ProjectileData _primaryProjectile;
+		[JsonProperty("secondary_projectile")]
+		private ProjectileData _secondaryProjectile;
+
+		[JsonProperty("cooldown")]
+		private readonly string _cooldown;
+		[JsonProperty("mana_cost")]
+		private readonly string _manaCost;
+
+		[JsonProperty("delay")]
+		private readonly string _delay;
+		[JsonProperty("N")]
+		private readonly string _n;
+		[JsonProperty("spray")]
+		private readonly string _spray;
+
     }
+
 }
