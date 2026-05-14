1124dd6a9af182ff83b88866974b6d00b578b0ef
Gloomsfield <amory@acosta.boo>
2026-05-13 20:00:56 -0700

    add fields to SpellBlueprint, start CastBehaviors

--- Assets/Scripts/Spells/SpellBlueprint.cs
+++ Assets/Scripts/Spells/SpellBlueprint.cs
@@ -2,13 +2,36 @@ using System.Collections.Generic;
 
 namespace Spells {
 
+	public abstract class CastBehavior { public virtual void Cast(); }
+
+	namespace CastBehaviors {
+
+		public class Simple : Base {
+
+			// TODO
+			public override void Cast() {
+
+			}
+
+		}
+
+	}
+
 	public class SpellBlueprint {
 		
+		public string name;
+		public string description;
+		public int icon;
+
+		private ProjectileBlueprint _primaryProjectileBlueprint;
+		private ProjectileBlueprint _secondaryProjectileBlueprint;
+
+		private CastBehavior _castBehavior;
+
 		private Dictionary<string, Stat> _stats;
 
+		// TODO
 		public void Modify(Modifier modifier) {}
-		
-		public void Cast() {}
 
 	}
 
