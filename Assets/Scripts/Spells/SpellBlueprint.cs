d8d632f17300c6049e4d0b1131845f9a83728f51
Gloomsfield <amory@acosta.boo>
2026-05-13 19:49:36 -0700

    begin writing SpellBlueprint class

---
+++ Assets/Scripts/Spells/SpellBlueprint.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Spells {
+
+	public class SpellBlueprint {
+		
+		private Dictionary<string, Stat> _stats;
+
+		public void Modify(Modifier modifier) {}
+		
+		public void Cast() {}
+
+	}
+
+}
