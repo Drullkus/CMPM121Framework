9868f4b2bb2c1614d1fb9ff2d60d16bad88a8c61
Gloomsfield <amory@acosta.boo>
2026-05-18 16:22:57 -0700

    add skeleton WaveManager class

---
+++ Assets/Scripts/Levels/WaveManager.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using System.Timers;
+
+public class WaveManager {
+
+	private Action<Spawn> _spawnAction;
+
+	// TODO
+	public void Reset() { }
+
+	// TODO
+	public void StartWave(Level level) { }
+
+	// TODO
+	public void EndWave() { }
+
+	// TODO
+	private void SetSpawnTimer(Spawn spawn) { }
+
+}
+
