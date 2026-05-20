5bc3cbedc489a7bee7711e25effdc76aa0e21d20
Gloomsfield <amory@acosta.boo>
2026-05-20 00:22:38 -0700

    write proper Show method for Reward screen

--- Assets/Scripts/UI/UIObject/Reward.cs
+++ Assets/Scripts/UI/UIObject/Reward.cs
@@ -1,15 +1,37 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Reward : MonoBehaviour {
 
+	[SerializeField]
+	private List<GameObject> _statDisplaySlots;
+
 	private void Awake() {
 		UIObject uiObject = new(
 			UIState.REWARD,
-			() => { gameObject.SetActive(true); },
+			Show,
 			() => { gameObject.SetActive(false); }
 		);
 
 		EventBus.Instance.RegisterUIObject(uiObject);
+	}
+
+	private void Show() {
+		List<string> stats = WaveStatTracker.Instance.GetRandomFormattedStats(_statDisplaySlots.Count);
+
+		foreach(GameObject statDisplaySlot in _statDisplaySlots) {
+			statDisplaySlot.SetActive(false);
+		}
+
+		for(int i = 0; i < stats.Count; i++) {
+			_statDisplaySlots[i].GetComponent<TextMeshProUGUI>().text = stats[i];
+
+			_statDisplaySlots[i].SetActive(true);
+		}
+
+		gameObject.SetActive(true);
 	}
 
 }
