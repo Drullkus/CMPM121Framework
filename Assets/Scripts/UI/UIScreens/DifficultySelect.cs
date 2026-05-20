f8092c5fb0f786a76cbef65babeb5ef307d0eecb
Gloomsfield <amory@acosta.boo>
2026-05-20 03:18:21 -0700

    write and use DifficultySelectButton class

--- Assets/Scripts/UI/UIScreens/DifficultySelect.cs
+++ Assets/Scripts/UI/UIScreens/DifficultySelect.cs
@@ -2,13 +2,30 @@ using Newtonsoft.Json;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultySelectButton : Button {
+
+	private TextMeshProUGUI _label;
+
+	public void Initialize(Vector2 position, Level level) {
+		_label.text = level.Name;
+
+		transform.localPosition = (Vector3)position;
+
+		onClick.AddListener(() => {
+			EventBus.Instance.ChooseDifficulty(level);
+		});
+	}
+
+}
 
 public class DifficultySelect : MonoBehaviour {
 	
 	private List<Level> _levels;
 
 	[SerializeField]
-	private List<TextMeshProUGUI> _levelLabels;
+	private DifficultySelectButton _buttonPrefab;
 
 	private void Awake() {
 		AssetManager.Instance.LoadJson("levels", (loadedJson) => {
@@ -21,10 +38,14 @@ public class DifficultySelect : MonoBehaviour {
 			() => { gameObject.SetActive(false); }
 		);
 
-		EventBus.Instance.RegisterUIObject(uiScreen);
+		EventBus.Instance.RegisterUIScreen(uiScreen);
 	}
 
 	private void Show() {
+		for(int i = 0; i < _levels.Count; i++) {
+			Instantiate(_buttonPrefab).Initialize(new(130.0f - i * 40.0f, 0.0f),_levels[i]);
+		}
+
 		gameObject.SetActive(true);
 	}
 
