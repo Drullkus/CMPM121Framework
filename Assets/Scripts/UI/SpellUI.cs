8cb34e537389f75b8a84ce47e62a21ff3fbfcc8e
Gloomsfield <amory@acosta.boo>
2026-05-20 13:03:55 -0700

    standardize formatting of SpellUI.cs

--- Assets/Scripts/UI/SpellUI.cs
+++ Assets/Scripts/UI/SpellUI.cs
@@ -3,49 +3,39 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.InputSystem;
 
-public class SpellUI : MonoBehaviour
-{
+public class SpellUI : MonoBehaviour {
     public GameObject icon;
     public RectTransform cooldown;
     public TextMeshProUGUI manacost;
     public TextMeshProUGUI damage;
     public GameObject highlight;
     public Spell spell;
-    float last_text_update;
+    float lastTextUpdate;
     const float UPDATE_DELAY = 1;
     public GameObject dropbutton;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        last_text_update = 0;
+    void Start() {
+        lastTextUpdate = 0;
     }
 
-    public void SetSpell(Spell spell)
-    {
+    public void SetSpell(Spell spell) {
         this.spell = spell;
         GameManager.Instance.spellIconManager.PlaceSprite(spell.GetIcon(), icon.GetComponent<Image>());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (spell == null) return;
-        if (Time.time > last_text_update + UPDATE_DELAY)
-        {
+    void Update() {
+        if (spell == null) { return; }
+        if (Time.time > lastTextUpdate + UPDATE_DELAY) {
             manacost.text = spell.GetManaCost().ToString();
             damage.text = spell.GetDamage().ToString();
-            last_text_update = Time.time;
+            lastTextUpdate = Time.time;
         }
         
         float since_last = Time.time - spell.last_cast;
         float perc;
-        if (since_last > spell.GetCooldown())
-        {
+        if (since_last > spell.GetCooldown()) {
             perc = 0;
-        }
-        else
-        {
+        } else {
             perc = 1-since_last / spell.GetCooldown();
         }
         cooldown.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 48 * perc);
