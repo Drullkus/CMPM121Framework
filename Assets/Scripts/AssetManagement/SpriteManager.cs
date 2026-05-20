7c447189a0208f55f3b85e7f735452841b7663bd
Gloomsfield <amory@acosta.boo>
2026-05-20 14:00:31 -0700

    hardcode the player sprite indices as well

--- Assets/Scripts/AssetManagement/SpriteManager.cs
+++ Assets/Scripts/AssetManagement/SpriteManager.cs
@@ -10,7 +10,7 @@ public class SpriteManager {
 				_instance = new();
 
 				AssetManager.Instance.LoadSprites("enemy_sprites", _instance.RegisterEnemySprites);
-				AssetManager.Instance.LoadSprites("player_sprites", _instance.RegisterPlayerSprites);
+				AssetManager.Instance.LoadSprites("enemy_sprites", _instance.RegisterPlayerSprites);
 				AssetManager.Instance.LoadSprites("relic_sprites", _instance.RegisterRelicSprites);
 				AssetManager.Instance.LoadSprites("spell_sprites", _instance.RegisterSpellSprites);
 			}
@@ -42,6 +42,12 @@ public class SpriteManager {
 		2198,
 	};
 
+	private static int[] _playerSpriteSheetIndices = {
+		65,
+		101,
+		109,
+	};
+
 	private Sprite[] _enemySprites;
 	private Sprite[] _playerSprites;
 	private Sprite[] _relicSprites;
@@ -52,7 +58,12 @@ public class SpriteManager {
 	}
 
 	private void RegisterPlayerSprites(Sprite[] sprites) {
-		_playerSprites = sprites;
+		List<Sprite> selectedSprites = new();
+		foreach(int index in _playerSpriteSheetIndices) {
+			selectedSprites.Add(sprites[index]);
+		}
+
+		_playerSprites = selectedSprites.ToArray();
 	}
 
 	private void RegisterRelicSprites(Sprite[] sprites) {
