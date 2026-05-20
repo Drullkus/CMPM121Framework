fbb1038db172e93d6146029322bce953944433aa
Gloomsfield <amory@acosta.boo>
2026-05-19 22:41:32 -0700

    inline enemyStats declaration

--- Assets/Scripts/Levels/EnemySpawner.cs
+++ Assets/Scripts/Levels/EnemySpawner.cs
@@ -56,9 +56,7 @@ public class EnemySpawner : MonoBehaviour {
     void SpawnEnemy(Vector3 initial_position, Spawn spawn, int waveIndex) {
         GameObject new_enemy = Instantiate(enemy, initial_position, Quaternion.identity);
 
-        EnemyStats enemyStats;
-
-        if(!GameManager.Instance.enemyStats.TryGetValue(spawn.Enemy, out enemyStats)) {
+        if(!GameManager.Instance.enemyStats.TryGetValue(spawn.Enemy, out EnemyStats enemyStats)) {
             Debug.LogError($"tried to spawn enemy of type \"{spawn.Enemy}\" when no such enemy type exists!");
             return;
         }
