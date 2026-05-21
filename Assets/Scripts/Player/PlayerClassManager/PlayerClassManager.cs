e71a2cb496d4b1e60b16e6a487719dd9042a0d06
Gloomsfield <amory@acosta.boo>
2026-05-21 16:20:18 -0700

    fix PlayerClassManager compilation errors

--- Assets/Scripts/Player/PlayerClassManager/PlayerClassManager.cs
+++ Assets/Scripts/Player/PlayerClassManager/PlayerClassManager.cs
@@ -1,22 +1,23 @@
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 public class PlayerClassManager {
 
-	private Dictionary<PlayerClassData> _classData;
+	private static Dictionary<string, PlayerClassData> _classData;
 
-	private static void HandleJsonLoaded(string json, Action<Dictionary<PlayerClassData>> onDeserialized) {
-		_classData = onDeserialized.Invoke(JsonConvert.DeserializeObject<List<PlayerClassData>>(json));
+	private static void HandleJsonLoaded(string json, Action<Dictionary<string, PlayerClassData>> onDeserialized) {
+		_classData = JsonConvert.DeserializeObject<Dictionary<string, PlayerClassData>>(json);
 		onDeserialized.Invoke(_classData);
 	}
 
-	public static void GetClasses(Action<PlayerClassData> onGotten) {
+	public static void GetClasses(Action<Dictionary<string, PlayerClassData>> onGotten) {
 		if(_classData != null) {
 			onGotten(_classData);
 			return;
 		}
 
-		AssetManager.LoadJson("classes", (json) => { HandleJsonLoaded(json, onGotten); });
+		AssetManager.Instance.LoadJson("classes", (json) => { HandleJsonLoaded(json, onGotten); });
 	}
 
 }
