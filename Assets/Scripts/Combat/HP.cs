25c3048150b16abbcf991d9c807f8f8180ee07fe
Gloomsfield <amory@acosta.boo>
2026-05-20 20:30:59 -0700

    rename event to OnExpended, add Recover methods

--- Assets/Scripts/Combat/HP.cs
+++ Assets/Scripts/Combat/HP.cs
@@ -2,19 +2,30 @@ using System;
 
 public class HP {
 	
+	private int _maxValue;
 	private int _value;
 
-	public event Action OnHPExpended;
+	public event Action OnExpended;
 
 	public virtual void TakeDamage(Damage damage) {
 		_value -= damage.amount;
 		if(_value <= 0) {
 			_value = 0;
-			OnHPExpended?.Invoke();
+			OnExpended?.Invoke();
 		}
 	}
 
+	public void Recover(int amount) {
+		_value += amount;
+		Math.Clamp(_value, 0, _maxValue);
+	}
+
+	public void RecoverFull() {
+		_value = _maxValue;
+	}
+
 	public HP(int value) {
+		_maxValue = value;
 		_value = value;
 	}
 
