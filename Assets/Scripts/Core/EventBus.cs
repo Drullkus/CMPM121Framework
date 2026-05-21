22c79e89c2f441cdb84245ebdf280586b7c2d5a9
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-05-20 23:24:51 -0700

    Add events useful for relics

--- Assets/Scripts/Core/EventBus.cs
+++ Assets/Scripts/Core/EventBus.cs
@@ -12,9 +12,33 @@ public class EventBus {
     }
 
     public event Action<Vector3, Damage, Hittable> OnDamage;
-    
     public void DoDamage(Vector3 where, Damage dmg, Hittable target) {
         OnDamage?.Invoke(where, dmg, target);
+    }
+    
+    public event Action<Unit> MovementStarted;
+    public void DoMovementStarted(Unit unit) {
+        MovementStarted?.Invoke(unit);
+    }
+    
+    public event Action<Unit> MovementStopped;
+    public void DoMovementStopped(Unit unit) {
+        MovementStopped?.Invoke(unit);
+    }
+    
+    public event Action<Unit, Unit> OnKill;
+    public void DoOnKill(Unit killer, Unit victim) {
+        OnKill?.Invoke(killer, victim);
+    }
+    
+    public event Action GameStarted;
+    public void DoGameStarted() {
+        GameStarted?.Invoke();
+    }
+    
+    public event Action GameStopped;
+    public void DoGameStopped() {
+        GameStopped?.Invoke();
     }
 
 }
