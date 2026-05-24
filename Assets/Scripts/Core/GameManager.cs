6ad656ae0d1984e3ce6bce74ef24b149d0d15856
Gloomsfield <amory@acosta.boo>
2026-05-23 19:20:58 -0700

    fix WaveManager instantiation

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// we need to instantiate the WaveManager singleton
-	// somewhere. this feels like an okay place for it,
-	// but suggestions are welcome.
-	// don't attempt to access this. ever. just get the
-	// static WaveManager.Instance if you need a reference
-	// to the WaveManager.
-	private WaveManager _tempWaveManager = new();
+	private WaveManager _waveManager = new();
+
+	private void Awake() {
+		_waveManager.Initialize();
+	}
 
 }
