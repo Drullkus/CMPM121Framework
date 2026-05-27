eb6d619e933435aa4ce82d5c6d4021d1e712af34
Gloomsfield <amory@acosta.boo>
2026-05-27 14:47:46 -0700

    add Spell class stubs

--- Assets/Scripts/Spells/Spell.cs
+++ Assets/Scripts/Spells/Spell.cs
@@ -1,5 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public enum SpellTraitType {
+	TRAJECTORY,
+	DAMAGE_TYPE,
+	RPN_FLOAT,
+}
+
+public class SpellTrait {
+	
+	public SpellTraitType type;
+
+	public string traitValue;
+
+}
+
 public class Spell {
 	
+	public string name;
+	public string description;
+	public int icon;
+
+	private Dictionary<string, SpellTrait> _traits;
+
+	// TODO
+	public List<(string, SpellTrait)> GetTraits(List<string> traitNames) { return null; }
+
+	public List<string> GetTraitNames() { return _traits.Keys.ToList(); }
+
 	// TODO
     public void Cast() {}
 
