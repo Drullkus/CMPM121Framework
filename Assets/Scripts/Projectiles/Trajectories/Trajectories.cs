fbaf72fc9e00d3b75c225ea53c75e9a1a89130e1
Gloomsfield <amory@acosta.boo>
2026-05-27 14:13:26 -0700

    remove old spell code

--- Assets/Scripts/Projectiles/Trajectories/Trajectories.cs
+++
@@ -1,6 +0,0 @@
-public enum Trajectory {
-	UNDEFINED,
-	STRAIGHT,
-	SPIRAL,
-	HOMING,
-}
--- Assets/Scripts/Spells/SpellDamageData.cs
+++
@@ -1,8 +0,0 @@
-namespace Spells
-{
-    public class SpellDamageData
-    {
-        public string amount { get; set; }
-        public string type { get; set; }
-    }
-}
