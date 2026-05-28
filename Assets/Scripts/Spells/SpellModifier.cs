fa2b2904f54656a67ca643e3567edabac708453e
Gloomsfield <amory@acosta.boo>
2026-05-27 21:42:51 -0700

    add SpellModifier constructor and change ModifySpell signature

--- Assets/Scripts/Spells/SpellModifier.cs
+++ Assets/Scripts/Spells/SpellModifier.cs
@@ -6,13 +6,23 @@ public class SpellModifier {
     
 	private Dictionary<string, Action<SpellTrait>> _traitModifications;
 
-	public void ModifySpell(Spell spell) {
+	public string name;
+	public string description;
+
+	public SpellModifier(string name, string description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public Spell ModifySpell(Spell spell) {
 		foreach(
 			(string traitName, SpellTrait trait) in
 			spell.GetTraits(_traitModifications.Keys.ToList())
 		) {
 			_traitModifications[traitName].Invoke(trait);
 		}
+
+		return spell;
 	}
 
 }
