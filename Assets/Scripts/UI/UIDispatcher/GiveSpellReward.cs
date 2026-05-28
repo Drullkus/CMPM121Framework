721cb90a1e43d4f8c8423d55fd6512d7323ae3fe
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-05-28 00:32:39 -0700

    Add button element for granting spell reward

---
+++ Assets/Scripts/UI/UIDispatcher/GiveSpellReward.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace UI.UIDispatcher {
+    public class GiveSpellReward : MonoBehaviour {
+        
+		public void RollOption() {
+			Debug.Log("TODO set icon, text, and assign spell");
+		}
+
+		public void OnClick() {
+			Debug.Log("TODO on-click behavior");
+		}
+    }
+}
