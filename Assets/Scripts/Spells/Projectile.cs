91461c59a6dead758f787498cd59dd939df12b9d
Gloomsfield <amory@acosta.boo>
2026-05-27 23:52:35 -0700

    add Projectile skeleton class

---
+++ Assets/Scripts/Spells/Projectile.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Projectile : MonoBehaviour {
+    
+	public Team team;
+
+	private ProjectileTrajectory trajectory;
+
+	public static void Spawn(Vector2 spawnPosition, ProjectileTrajectory trajectory, int spriteIndex, Team team, Action<Projectile> onSpawn) {
+		AssetManager.Instance.LoadPrefab("projectile", (loadedPrefab) => {
+			Projectile newProjectile = Instantiate(loadedPrefab, spawnPosition, Quaternion.identity).GetComponent<Projectile>();
+
+			newProjectile.GetComponent<SpriteRenderer>().sprite = SpriteManager.Instance.RetrieveSpellSprite(spriteIndex);
+
+			newProjectile.team = team;
+			newProjectile.trajectory = trajectory;
+		});
+	}
+
+	private void Update() {
+		
+	}
+
+}
