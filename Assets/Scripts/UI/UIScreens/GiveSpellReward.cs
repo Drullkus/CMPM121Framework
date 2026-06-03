9eeecd41cdba8913ddeb8c0a3627d3b1db1b25df
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-06-02 19:46:07 -0700

    Hide spell reward button when collecting it

--- Assets/Scripts/UI/UIScreens/GiveSpellReward.cs
+++ Assets/Scripts/UI/UIScreens/GiveSpellReward.cs
@@ -6,21 +6,27 @@ using UnityEngine.UI;
 public class GiveSpellReward : MonoBehaviour {
 
     private Spell _spell;
-        
-    public void RollOption() {
-        //_spell = SpellBuilder.GenerateSpell();
-        //DisplaySpell();
-    }
 
-    private void DisplaySpell() {
+    private SpellBuilder _spellBuilder = new SpellBuilder();
+
+    [Header("Hidden upon collecting spell reward")]
+    [SerializeField] GameObject deactivatedUponClick;
+
+    public void RollOption() {
+        deactivatedUponClick.SetActive(true);
+        _spell = _spellBuilder.GenerateSpell(out string modifierInfo);
+
         var image = gameObject.GetComponentsInChildren<Image>().FirstOrDefault(c => c.gameObject != gameObject);
         image.sprite = SpriteManager.Instance.RetrieveSpellSprite(_spell.icon);
         
         var textMesh = gameObject.GetComponentInChildren<TextMeshProUGUI>();
         textMesh.text = $"{_spell.name}\n{_spell.description}"; // TODO add more info, like modifiers
+        textMesh.text += $"\n{modifierInfo}";
     }
 
     public void OnClick() {
         Debug.Log("TODO add _spell into player's spell deck");
+
+        deactivatedUponClick.SetActive(false);
     }
 }
